ba6150596616dc53810726d3e32046d64a497ffa
Melike Sivrikaya <melike.svrkya@gmail.com>
2024-08-20 00:28:27 +0300

    Explore service added

---
+++ Backend/explore-service/src/main/java/com/melikesivrikaya/exploreservice/ExploreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.melikesivrikaya.exploreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreServiceApplication.class, args);
+    }
+
+}
---
+++ Backend/explore-service/src/test/java/com/melikesivrikaya/exploreservice/ExploreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melikesivrikaya.exploreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
